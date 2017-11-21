130e4caade0addabc7bf6fec97defc576567d645
4pydev <4pydev@gmail.com>
2017-11-22 01:27:06 +0300

    Changed some func

--- check_sites_health.py
+++ check_sites_health.py
@@ -16,40 +16,51 @@ def load_urls4check(path):
         return urls_as_list
 
 
-def is_server_respond_with_200(url):
+def get_site_status(url):
     try:
         if requests.get(url).ok:
-            return True
+            return "OK"
     except RequestException:
-        print("{} - invalid URL\n".format(url))
+        return "Not OK"
     else:
-        print("{}: not OK".format(url))
-        return False
+        #print("{}: not OK".format(url))
+        return "Not OK"
 
 
 def get_domain_name(url):
-    return re.search(r'\w+\.\w+', url).group(0)
+    try:
+        return re.search(r'\w+\.\w+', url).group(0)
+    except AttributeError:
+        return None
 
 
 def get_domain_expiration_status(domain_name):
-    args = ['whois', domain_name]
-    process = subprocess.Popen(args, stdout=subprocess.PIPE)
-    whois_msg = process.communicate()[0].decode('utf-8')
-    expiry_date = re.findall(r'Registry Expiry Date: (\d{4}-\d{2}-\d{2})',
-                             whois_msg)[0]
-    return "OK" if expiry_date > str(date.today() +
-                                     timedelta(days=30)) else "Not OK"
+    try:
+        args = ['whois', domain_name]
+        process = subprocess.Popen(args, stdout=subprocess.PIPE)
+        whois_msg = process.communicate()[0].decode('utf-8')
+        expiry_date = re.findall(r'Registry Expiry Date: (\d{4}-\d{2}-\d{2})',
+                                 whois_msg)[0]
+        return "OK" if expiry_date > str(date.today() +
+                                         timedelta(days=30)) else "Not OK"
+    except TypeError:
+        return "Not OK"
 
+
+def print_site_status(url, site_status, expiry_status):
+    print("{site_url}: {site_status}\nExpiry date check: {expiry_status}\n"
+          .format(site_url=url,
+                  site_status=site_status,
+                  expiry_status=expiry_status))
 
 if __name__ == '__main__':
     try:
         urls_list = load_urls4check(sys.argv[1])
         for current_url in urls_list:
-            if is_server_respond_with_200(current_url):
-                print("{site_url}: OK\nExpiry date check: {expiry_date}\n"
-                      .format(site_url=current_url,
-                              expiry_date=get_domain_expiration_status(
-                                  get_domain_name(current_url))))
+            print_site_status(url=current_url,
+                              site_status=get_site_status(current_url),
+                              expiry_status=get_domain_expiration_status(
+                                  get_domain_name(current_url)))
     except IndexError:
         print("You must enter a filename.")
     except FileNotFoundError:
