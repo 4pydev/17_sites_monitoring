1fd938637c3a44b51442feaeee5ab1a04f217ff0
4pydev <4pydev@gmail.com>
2017-11-21 23:19:04 +0300

    Fixed check_sites_health.py

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from sys import exit
+import sys
 from datetime import date, timedelta
 import subprocess
 import re
 import requests
-from requests.exceptions import MissingSchema
+from requests.exceptions import RequestException
 
 
 def load_urls4check(path):
@@ -16,14 +16,14 @@ def load_urls4check(path):
             urls_as_list = text_urls.split('\n')
             return urls_as_list
     except FileNotFoundError:
-        exit('Enter a valid path or filename.')
+        sys.exit('Enter a valid path or filename.')
 
 
 def is_server_respond_with_200(url):
     try:
         if requests.get(url).status_code == 200:
             return True
-    except MissingSchema:
+    except RequestException:
         print("{} - invalid URL\n".format(url))
     else:
         print("{}: not OK".format(url))
@@ -44,15 +44,18 @@ def get_domain_name(url):
 
 
 if __name__ == '__main__':
-    urls_list = load_urls4check('urls.txt')
-    for current_url in urls_list:
-        if is_server_respond_with_200(current_url):
-            current_domain_name = get_domain_name(current_url)
-            domain_expire_date = get_domain_expiration_date(
-                                                current_domain_name)
-            if domain_expire_date > str(date.today()+timedelta(days=30)):
-                exp_date_check = "OK"
-            else:
-                exp_date_check = "Not OK"
-            print("{site_url}: OK\nExpiry date check: {expiry_date}\n"
-                  .format(site_url=current_url, expiry_date=exp_date_check))
+    if len(sys.argv) > 1:
+        urls_list = load_urls4check(sys.argv[1])
+        for current_url in urls_list:
+            if is_server_respond_with_200(current_url):
+                current_domain_name = get_domain_name(current_url)
+                domain_expire_date = get_domain_expiration_date(
+                                                    current_domain_name)
+                if domain_expire_date > str(date.today()+timedelta(days=30)):
+                    exp_date_check = "OK"
+                else:
+                    exp_date_check = "Not OK"
+                print("{site_url}: OK\nExpiry date check: {expiry_date}\n"
+                      .format(site_url=current_url, expiry_date=exp_date_check))
+    else:
+        print("You must enter a filename.")
